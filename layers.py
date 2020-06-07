e527d255c6f7c22439617ac7af44a4bbfb2762d6
Anish <josh.anish1@gmail.com>
2020-06-07 17:21:50 +0530

    Update layers.py
    shape_dim resolve

--- layers.py
+++ layers.py
@@ -16,36 +16,37 @@ def self_refinement(inps,d):
     out = Activation("relu")(out)
     return out
 
+
 def head_attention(inps):
-	x1 = Conv2D(256,3,padding="same")(inps)
-	x1 = BatchNormalization(axis=-1)(x1)
-	x1 = Activation("relu")(x1)
-	x2 = Conv2D(512,3,padding="same")(x1)
-	w,b = tf.split(x2,2,axis=-1)
-	out = Multiply()([x1,w])
-	out = Add()([out,b])
-	F   = Activation("relu")(out)
-	ave = tf.math.reduce_mean(F,axis=-1)
-	ave = tf.expand_dims(ave,axis=-1)
-	
-	ave = Conv2D(256,1,padding="same",activation="relu")(ave)
-	Fn  = Conv2D(256,1,padding="same",activation="sigmoid")(ave)
-	out = Multiply()([F,Fn])
-	return out
+    x1 = Conv2D(256,3,padding="same")(inps)
+    x1 = BatchNormalization(axis=-1)(x1)
+    x1 = Activation("relu")(x1)
+    x2 = Conv2D(512,3,padding="same")(x1)
+    w,b = tf.split(x2,2,axis=-1)
+    out = Multiply()([x1,w])
+    out = Add()([out,b])
+    F   = Activation("relu")(out)
+    ave = tf.math.reduce_mean(F,axis=-1)
+    ave = tf.expand_dims(ave,axis=-1)
+    
+    ave = Conv2D(256,1,padding="same",activation="relu")(ave)
+    Fn  = Conv2D(256,1,padding="same",activation="sigmoid")(ave)
+    out = Multiply()([F,Fn])
+    return out
 
 
 def GCF(inps):
-	x1 = Conv2D(256,3,padding="same")(inps)
-	x1 = BatchNormalization(axis=-1)(x1)
-	x1 = Activation("relu")(x1)
-	
-	gap = tf.math.reduce_mean(x1,axis=-1)
-	gap = tf.expand_dims(gap,axis=-1)
-	gap = Conv2D(256,1,padding="same",activation="relu")(gap)
-	gap = Conv2D(256,1,padding="same",activation="sigmoid")(gap)
+    x1 = Conv2D(256,3,padding="same")(inps)
+    x1 = BatchNormalization(axis=-1)(x1)
+    x1 = Activation("relu")(x1)
+    
+    gap = tf.math.reduce_mean(x1,axis=-1)
+    gap = tf.expand_dims(gap,axis=-1)
+    gap = Conv2D(256,1,padding="same",activation="relu")(gap)
+    gap = Conv2D(256,1,padding="same",activation="sigmoid")(gap)
 
-	out = Multiply()([gap,x1])
-	return out
+    out = Multiply()([gap,x1])
+    return out
 
 
 def FIA(hin,gin,tin,ft):
@@ -53,19 +54,19 @@ def FIA(hin,gin,tin,ft):
     fl = BatchNormalization(axis=-1)(fl)
     fl = Activation("relu")(fl)
     
-    fh = Conv2D(ft,3,padding="same")(hin)
+    fh = Conv2D(ft,3,padding="same")(hin)# left block
     fh = Activation("relu")(fh)
-    fh = UpSampling2D(interpolation="bilinear")(fh)
-    fhl  = Multiply()([fl,fh]) 
+    fh = UpSampling2D()(fh)
+    fhl  = Multiply()([fl,fh]) #--->
     
-    fdh = UpSampling2D()(hin)  
+    fdh = UpSampling2D()(hin)  #central block
     flh = Conv2D(ft,3,padding="same")(fl)
     flh = Activation("relu")(flh)
-    flh = Multiply()([fdh,flh]) 
+    flh = Multiply()([fdh,flh]) #--->
     
     fg = Conv2D(ft,3,padding="same")(gin)
     fg = Activation("relu")(fg)
-    fg = UpSampling2D(interpolation="bilinear")(fg)
+    fg = UpSampling2D()(fg)
     fgl = Multiply()([fg,fl])
     
     conc = Concatenate()([fhl,flh,fgl])
@@ -77,16 +78,16 @@ def FIA(hin,gin,tin,ft):
 
 
 def GCPA():
-    ENCODER_BASE = tf.keras.applications.ResNet50(include_top=False,input_shape=(config_map["dim"],config_map["dim"],3))
+    ENCODER_BASE = tf.keras.applications.ResNet50(include_top=False,input_shape=(None,None,3))
     for layer in ENCODER_BASE.layers:
         layer.traiable = True
+        
+    f0 = ENCODER_BASE.get_layer("conv1_relu").output  #activation conv1_relu
 
-    f0 = ENCODER_BASE.get_layer("conv1_relu").output      
-
-    f1 = ENCODER_BASE.get_layer("conv2_block2_out").output   
-    f2 = ENCODER_BASE.get_layer("conv3_block4_out").output   
+    f1 = ENCODER_BASE.get_layer("conv2_block2_out").output  
+    f2 = ENCODER_BASE.get_layer("conv3_block4_out").output 
     f3 = ENCODER_BASE.get_layer("conv4_block6_out").output  
-    f4 = ENCODER_BASE.get_layer("conv5_block3_out").output  
+    f4 = ENCODER_BASE.get_layer("conv5_block3_out").output 
     
     aux1 = head_attention(f4)
     aux1 = self_refinement(aux1,1) 
@@ -115,8 +116,15 @@ def GCPA():
     dom0 = Conv2D(64,3,padding="same")(dom0)
     dom0 = BatchNormalization(axis=-1)(dom0)
     dom0 = Activation("relu")(dom0)
+    
+    dom0 = Concatenate(axis=-1)([dom0,f0])
+    dom0 = Conv2D(64,3,padding="same")(dom0)
+    dom0 = BatchNormalization(axis=-1)(dom0)
+    dom0 = Activation("relu")(dom0)
+    
     dom0 = UpSampling2D(interpolation="bilinear")(dom0)
 
+    
     out  = Conv2D(1,3,padding="same")(dom0)
     out  = Activation("sigmoid",name="dom")(out)
     
